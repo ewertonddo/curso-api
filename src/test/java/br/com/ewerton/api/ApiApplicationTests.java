1ee5e137b0172f1dc2e775695d97190b9904ff1a
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-02-02 19:08:02 -0300

    projeto iniciado

---
+++ src/test/java/br/com/ewerton/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
