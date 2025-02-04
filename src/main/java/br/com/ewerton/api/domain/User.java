7dace0124dd06160095d3d0ed5caf9021789bb48
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-02-04 00:20:58 -0300

    classe usuario

---
+++ src/main/java/br/com/ewerton/api/domain/User.java
@@ -0,0 +1,23 @@
+package br.com.ewerton.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
