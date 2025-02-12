994a8727ad2f9ee7fd38827205e5583bcab0f20d
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-02-11 22:46:40 -0300

    tratamento de excecao para objeto nao encontrato

--- src/main/java/br/com/ewerton/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/ewerton/api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.ewerton.api.service.impl;
 import br.com.ewerton.api.domain.Users;
 import br.com.ewerton.api.repositories.UserRepository;
 import br.com.ewerton.api.service.UserService;
+import br.com.ewerton.api.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public Users findById(Integer id) {
         Optional<Users> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow( () -> new ObjectNotFoundException("Object not found!"));
     }
 }
