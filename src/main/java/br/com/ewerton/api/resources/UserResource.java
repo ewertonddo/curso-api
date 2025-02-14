b2703bc3273161fcc9f12c2f233fec6fdcd44c75
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-02-14 00:43:16 -0300

    retornando dto para user resource

--- src/main/java/br/com/ewerton/api/resources/UserResource.java
+++ src/main/java/br/com/ewerton/api/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.ewerton.api.resources;
 
-import br.com.ewerton.api.domain.Users;
+import br.com.ewerton.api.domain.dto.UsersDTO;
 import br.com.ewerton.api.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Users> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UsersDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UsersDTO.class));
     }
 }
